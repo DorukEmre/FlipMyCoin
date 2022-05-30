d23b82e7bfcda93903feb00d956088f9f07af1ee
DorukEmre <doruk.emre@outlook.com>
2022-05-30 17:26:48 +0100

    Images served when button clicked

--- server.js
+++ server.js
@@ -15,20 +15,54 @@ const server = http.createServer((req, res) => {
     });
   }
   else if (page == '/api') {
-    // code to flip a coin and return flipped coin
+    let flipRes = Math.ceil(Math.random() * 2) === 1 ? 'Heads' : 'Tails'
+    if(flipRes == 'Heads'){
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      const objToJson = {
+        name: flipRes,
+        image: '/assets/Heads-Coin_of_Julius_Caesar_Sacred_Palladium.jpg',
+        imageAlt: "Heads side of coin"
+      }
+      res.end(JSON.stringify(objToJson));
+    }//student = leon
+    else if(flipRes == 'Tails'){
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      const objToJson = {
+        name: flipRes,
+        image: '/assets/Tails-Coin_of_Julius_Caesar_Sacred_Palladium.jpg',
+        imageAlt: "Tails side of coin"
+      }
+      res.end(JSON.stringify(objToJson));
+    }
   }
   else if (page == '/css/style.css'){
     fs.readFile('css/style.css', function(err, data) {
       res.write(data);
       res.end();
     });
-  }else if (page == '/js/main.js'){
+  }
+  else if (page == '/js/main.js'){
     fs.readFile('js/main.js', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/javascript'});
       res.write(data);
       res.end();
     });
-  }else{
+  }
+  else if (page == '/assets/Heads-Coin_of_Julius_Caesar_Sacred_Palladium.jpg'){
+    fs.readFile('./assets/Heads-Coin_of_Julius_Caesar_Sacred_Palladium.jpg', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'image/jpg' });
+       res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/assets/Tails-Coin_of_Julius_Caesar_Sacred_Palladium.jpg'){
+    fs.readFile('./assets/Tails-Coin_of_Julius_Caesar_Sacred_Palladium.jpg', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'image/jpg' });
+       res.write(data);
+      res.end();
+    });
+  }
+  else{
   
   }
 });
